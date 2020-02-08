5b9fe28be36c076233e73c69ecfb81eafdb29f43
Adam Rauch <adam@labkey.com>
2020-02-07 16:25:04 -0800

    Migrate to getSchemaVersion(). Remove ConsolidateScripts.

--- demo/src/org/labkey/demo/DemoModule.java
+++ demo/src/org/labkey/demo/DemoModule.java
@@ -17,6 +17,7 @@
 package org.labkey.demo;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.labkey.api.data.Container;
 import org.labkey.api.data.ContainerManager;
 import org.labkey.api.module.DefaultModule;
@@ -47,7 +48,7 @@ public class DemoModule extends DefaultModule
     }
 
     @Override
-    public double getVersion()
+    public @Nullable Double getSchemaVersion()
     {
         return 20.000;
     }
