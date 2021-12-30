1e891ad3c83dfd11d878cdaae4f62a5c5d17d888
Adam Rauch <adam@labkey.com>
2021-12-30 13:09:22 -0800

    Bump all schema versions to 22.000

--- demo/src/org/labkey/demo/DemoModule.java
+++ demo/src/org/labkey/demo/DemoModule.java
@@ -50,7 +50,7 @@ public class DemoModule extends DefaultModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 21.000;
+        return 22.000;
     }
 
     @Override
