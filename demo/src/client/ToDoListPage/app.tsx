07c9309bedb32ea89e05d98acc39e2cd543f07c0
Adam Rauch <adam@labkey.com>
2023-06-13 12:52:42 -0700

    Eliminate demo module

--- demo/src/client/ToDoListPage/app.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import { ToDoListPage } from './ToDoListPage'
-
-// Need to wait for container element to be available in labkey wrapper before render
-window.addEventListener('DOMContentLoaded', (event) => {
-    ReactDOM.render(<ToDoListPage />, document.getElementById('app'));
-});
--- demo/src/client/ToDoListPage/webpart/app.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from '@labkey/api';
-
-import { ToDoListPage } from '../ToDoListPage';
-
-App.registerApp<any>('demoWebpart', target => {
-    ReactDOM.render(<ToDoListPage />, document.getElementById(target));
-});
