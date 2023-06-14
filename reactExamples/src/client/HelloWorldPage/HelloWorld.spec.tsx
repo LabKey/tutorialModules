44ebc88376af7da735a9e5d2986745c357e79451
cnathe <cnathe@labkey.com>
2023-06-14 17:10:58 -0500

    remove jest snapshot check

--- reactExamples/src/client/HelloWorldPage/HelloWorld.spec.tsx
+++ reactExamples/src/client/HelloWorldPage/HelloWorld.spec.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import toJson from 'enzyme-to-json';
 import { App } from './HelloWorld';
 
 describe('HelloWorld', () => {
@@ -14,8 +13,6 @@ describe('HelloWorld', () => {
         expect(helloWorldSpan.length).toEqual(1);
         expect(helloWorldSpan.text()).toEqual('Hello World!');
 
-        // Verify snapshot
-        expect(toJson(helloWorld)).toMatchSnapshot();
         helloWorld.unmount();
     });
 });
