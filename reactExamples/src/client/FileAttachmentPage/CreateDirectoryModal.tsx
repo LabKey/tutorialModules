78464b08785916547abb2fda2d4a4e6d7c0464fe
cnathe <cnathe@labkey.com>
2023-06-14 16:50:22 -0500

    migrate react examples / pages from tutorialModules/demo to new reactExamples module

---
+++ reactExamples/src/client/FileAttachmentPage/CreateDirectoryModal.tsx
@@ -0,0 +1,53 @@
+import React, { FC, memo, useCallback, useState } from 'react';
+import { Button, Col, Form, FormControl, Modal, Row } from 'react-bootstrap';
+
+interface Props {
+    close: () => void;
+    submit: (name: string) => void;
+}
+
+export const CreateDirectoryModal : FC<Props> = memo(props => {
+    const { close, submit } = props;
+    const [name, setName] = useState<string>('');
+
+    const onChange = useCallback((evt: any) => {
+        setName(evt.target.value);
+    }, []);
+
+    const _submit = useCallback((evt: any) => {
+        submit(name?.trim());
+    }, [submit, name]);
+
+    return (
+        <Modal show={true} onHide={close}>
+            <Modal.Header>Create Directory</Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Row className="form-group">
+                        <Col xs={4}>
+                            <div>Directory Name:</div>
+                        </Col>
+                        <Col xs={8}>
+                            <FormControl type="text" id={'directory-name'} name={'directory-name'} onChange={onChange} />
+                        </Col>
+                    </Row>
+                </Form>
+                <Row>
+                    <Col xs={12}>
+                        <Button onClick={close} className="pull-left">
+                            Cancel
+                        </Button>
+                        <Button
+                            className="pull-right"
+                            bsStyle="success"
+                            disabled={name?.trim().length === 0}
+                            onClick={_submit}
+                        >
+                            Submit
+                        </Button>
+                    </Col>
+                </Row>
+            </Modal.Body>
+        </Modal>
+    )
+});
