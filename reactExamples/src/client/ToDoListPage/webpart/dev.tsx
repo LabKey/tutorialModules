86b3505803a3fdd732df621811634cccac03b556
Alan Vezina <alanv@labkey.com>
2023-12-20 16:30:43 -0600

    Bump components, and build, update dev.tsx files

--- reactExamples/src/client/ToDoListPage/webpart/dev.tsx
+++ reactExamples/src/client/ToDoListPage/webpart/dev.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
 import { App } from '@labkey/api';
 
 import { ToDoListPage } from "../ToDoListPage";
 
 App.registerApp<any>('demoWebpart', (target: string) => {
-    ReactDOM.render(
-        <AppContainer>
-            <ToDoListPage />
-        </AppContainer>,
-        document.getElementById(target)
-    );
+    ReactDOM.render(<ToDoListPage />, document.getElementById(target));
 }, true /* hot */);
-
-declare const module: any;
