92d715aea78e3130cb45b238fc812e85bddeb953
Cory Nathe <cnathe@labkey.com>
2024-07-10 12:45:11 -0500

    Update entryPoints.js to use permissionClasses instead of permission property (#191)
    - Update entryPoints.js to use permissionClasses instead of permission property
    - Remove entryPoints.js permission props for generateLib since they don't apply for libs

--- reactExamples/src/client/entryPoints.js
+++ reactExamples/src/client/entryPoints.js
@@ -7,28 +7,27 @@ module.exports = {
     apps: [{
         name: 'helloWorld',
         title: 'Hello World Page',
-        permission: 'read',
+        permissionClasses: ['org.labkey.api.security.permissions.ReadPermission'],
         path: './src/client/HelloWorldPage'
     },{
         name: 'todoList',
         title: 'To-Do List Page',
-        permission: 'insert',
+        permissionClasses: ['org.labkey.api.security.permissions.InsertPermission'],
         path: './src/client/ToDoListPage'
     },{
         name: 'demoWebpart',
         title: 'To-Do List Webpart',
-        permission: 'read',
         path: './src/client/ToDoListPage/webpart',
         generateLib: true // used by views/demoWebpart.html
     },{
         name: 'queryModel',
         title: 'QueryModel Example Page',
-        permission: 'read',
+        permissionClasses: ['org.labkey.api.security.permissions.ReadPermission'],
         path: './src/client/QueryModelPage'
     },{
         name: 'fileAttachmentForm',
         title: 'File Attachment Example Page',
-        permission: 'insert',
+        permissionClasses: ['org.labkey.api.security.permissions.InsertPermission'],
         path: './src/client/FileAttachmentPage'
     }]
 };
