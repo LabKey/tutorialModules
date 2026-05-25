269cb96a3e3931fb8df5fcb601f031f97a5501c4
Adam Rauch <adam@labkey.com>
2026-05-25 15:18:30 -0700

    Update copyrights and licenses

--- reactExamples/src/client/ToDoListPage/ToDoForm.tsx
+++ reactExamples/src/client/ToDoListPage/ToDoForm.tsx
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2023-2026 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 import React, { FC, memo, useCallback, ChangeEvent } from 'react';
 
 interface Props {
--- sourdough/resources/queries/lists/RisesQuery.sql
+++ sourdough/resources/queries/lists/RisesQuery.sql
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2023-2026 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 SELECT Rises.X,
        Rises.starter1,
        Rises.starter2,
